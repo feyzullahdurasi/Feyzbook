41ff4735424053812ccca4cf5b42ca392f804381
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-05-14 22:06:23 +0300

    firebase eklendi ve feed, Upload ve Settings sayfaları tabbar ile oluşturldu. Giriş sayfası hazırlandı. Resim ve Yorum yükleme özelliği yapıldı.

--- Feyzbook/UploadViewController.swift
+++ Feyzbook/UploadViewController.swift
@@ -6,24 +6,90 @@
 //
 
 import UIKit
+import Firebase
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var yorumTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselSec))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func gorselSec() {
+        let pickerContoller = UIImagePickerController()
+        pickerContoller.delegate = self
+        pickerContoller.sourceType = .photoLibrary
+        present(pickerContoller, animated: true, completion: nil)
     }
-    */
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imageView.image = image
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func uploadButtonTiklandi(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReferance = storage.reference()
+        let mediaFolder = storageReferance.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            
+            imageReference.putData(data, metadata: nil) { (storagemetadata, error) in
+                if error != nil {
+                    self.hataMesajGoster(title: "Hata", message: error?.localizedDescription ?? "hata Aldınız")
+                } else {
+                    imageReference.downloadURL { (url, error) in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            
+                            if let imageUrl = imageUrl {
+                                
+                                let firestoreDatabase = Firestore.firestore()
+                                
+                                let firestorePost = ["gorselUrl" : imageUrl, "yorum" : self.yorumTextField.text!, "email" : Auth.auth(), "tarih" : FieldValue.serverTimestamp() ] as [String : Any]
+                                
+                                firestoreDatabase.collection("Post").addDocument(data: firestorePost) { (error) in
+                                    if error != nil {
+                                        self.hataMesajGoster(title: "Hata", message: error?.localizedDescription ?? "Hata aldiniz")
+                                    }
+                                }
+                            }
+                            
+                            
+                            
+                        }
+                    }
+                }
+                
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    func hataMesajGoster(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+        
+    }
+    
 }
--- Feyzbook/ViewController.swift
+++ Feyzbook/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sifreTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func girisYapTiklandi(_ sender: Any) {
+        
+        if emailTextField.text != "" && sifreTextField.text != "" {
+            
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: sifreTextField.text!) { AuthDataResult, error in
+                if error != nil {
+                    self.hataMesaji(titleInput: "Hata!", messageInput: "Kullanıcı Adı ve Şifre Giriniz!")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        } else {
+            hataMesaji(titleInput: "Hata!", messageInput: "Kullanıcı Adı ve Şifre Giriniz!")
+        }
+        
+    }
+    
+    @IBAction func kayitOlTiklandi(_ sender: Any) {
+        
+        if emailTextField.text != "" && sifreTextField.text != "" {
+            // kayit işlemi
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: sifreTextField.text!) { authdataresult, error in
+                if error != nil {
+                    self.hataMesaji(titleInput: "Hata", messageInput: error?.localizedDescription ?? "Hata aldınız, tekrar deneyiniz")
+                    
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+                
+            }
+        } else {
+            hataMesaji(titleInput: "Hata!", messageInput: "Kullanıcı Adı ve Şifre Giriniz!")
+        }
+        
+    }
+    func hataMesaji(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+        
+    }
 }
 
